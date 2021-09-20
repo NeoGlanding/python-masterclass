af5284b31b1016f2b3bd397236243a7e300b1dd8
NeoGlanding <neoglanding@protonmail.com>
2021-09-20 22:28:47 +0700

    section 2

--- 01-python-introduction/string-formatting.py
+++ 01-python-introduction/string-formatting.py
@@ -4,3 +4,7 @@
 
 for i in range(1,11):
     print("No {0:8}, cubed is {1:>9}, squared is {2}".format(i, i ** 2, i **3))
+
+# f-strings, sama seperti backtick pada js, untuk melakukan formatting
+nama = "Ihsan"
+print(f"Hallo nama saya {nama}")
