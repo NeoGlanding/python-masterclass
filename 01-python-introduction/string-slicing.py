dd5145a5b5742ae9014bc14aa55c0971bb79624c
NeoGlanding <neoglanding@protonmail.com>
2021-09-20 17:17:51 +0700

    string slicing

---
+++ 01-python-introduction/string-slicing.py
@@ -0,0 +1,13 @@
+# Slicing adalah proses mengambil string bedasarkan start and stop
+
+nama = "Muhammad Ihsan Abdurrahman"
+
+# Mengambil nama dari index 0, dan berakhir di 2 (3 tidak termasuk)
+print(nama[0:3]) #Muh
+
+# Mengambil nama dari index ke 5, sampai akhir
+print(nama[3:]) # ammad Ihsan Abdurrahman
+
+# Mengambil dari index 0 sampai index 10 (11 tidak termasuk)
+print(nama[:11])
+
