b0b1e032bc36b154ac23f76dd6c56238e6db4653
NeoGlanding <neoglanding@protonmail.com>
2021-09-20 18:13:15 +0700

    string slicing with step

--- 01-python-introduction/string-slicing.py
+++ 01-python-introduction/string-slicing.py
@@ -11,3 +11,8 @@ print(nama[3:]) # ammad Ihsan Abdurrahman
 # Mengambil dari index 0 sampai index 10 (11 tidak termasuk)
 print(nama[:11])
 
+# Mengambil dari index ke 4 dari belakang sampai ke 3 dari belakang (2 tidak termasuk)
+print(nama[-4:-2])
+
+# Mengambil dari awal sampai akhir, tapi diambil setiap dua langkah
+print(nama[0::2]) #Mhma ha burha
