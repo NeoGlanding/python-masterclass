d90e49f34969311b6f48ae53d62e46b949fa447b
NeoGlanding <neoglanding@protonmail.com>
2021-09-20 22:43:52 +0700

    code-blocks

---
+++ 02-program-flow/code-blocks.py
@@ -0,0 +1,6 @@
+# Code blocks dipisahkan dengan indentasi
+
+for i in range(1,11):
+    print(f"This is number {i}") #print ini masuk kedalam code block for
+
+print("*" * 80) #di luar dari code block
