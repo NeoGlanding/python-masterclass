9f4851264b2b4797d8a6776b484b11d79a488ad3
NeoGlanding <neoglanding@protonmail.com>
2021-09-20 14:32:53 +0700

    variable and type

---
+++ 01-python-introduction/variables-and-types.py
@@ -0,0 +1,11 @@
+# Cara untuk mendeklarasikan variabel dan melakukan pengisian value
+nama = "Ihsan"
+usia = 13
+
+# Melakukan print untuk variabel
+print(nama)
+print(usia)
+
+# Melakukan pengecekan tipe data dalam variabel
+print(type(nama))
+print(type(usia))
