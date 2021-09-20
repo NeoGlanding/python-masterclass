934defed0a46d572067cd2f48748717539457874
NeoGlanding <neoglanding@protonmail.com>
2021-09-20 18:58:40 +0700

    string replacement fields

---
+++ 01-python-introduction/string-replacement-fields.py
@@ -0,0 +1,7 @@
+nama = "Muhammad Ihsan"
+usia = 18
+sekolah = 'Impact Byte'
+
+# Mereplace index yang ada pada string dengan value yang kita inginkan
+print("Nama saya {0} saya berusia {1} tahun, dan bersekolah di {2}"
+    .format(nama, usia, sekolah))
