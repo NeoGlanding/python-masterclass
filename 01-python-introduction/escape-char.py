a85b00e232aab11575cc622df20a48b0e7e37584
NeoGlanding <neoglanding@protonmail.com>
2021-09-20 14:07:55 +0700

    escape character part 1

---
+++ 01-python-introduction/escape-char.py
@@ -0,0 +1,13 @@
+# Melakukan seperti enter dengan escape character \
+splitString = "Hello There \nthis is escaped string \nwith \\n"
+print(splitString)
+
+# Melakukan insert "" di dalam " "
+escapeCharacter = "Ihsan said \"Hello There\""
+print(escapeCharacter)
+
+# String modern dengan kemampuan dapat melakukan multi line dan escaping sendiri, dengan 3 quore
+anotherLevelOfString = """Hello There "This is what ihsan said" 
+I was said that
+"""
+print(anotherLevelOfString)
