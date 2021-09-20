4a7e1caedadf19db8843495f781f22e6aa1c256c
NeoGlanding <neoglanding@protonmail.com>
2021-09-20 16:48:10 +0700

    string indexing

---
+++ 01-python-introduction/string-data-type.py
@@ -0,0 +1,18 @@
+# String adalah tipe data yang termasuk sequence / sekuensial, karena pada dasarnya string adalah
+# gabungan dari beberapa karakter / sequence of character
+
+rabbit = "Jacko Sulaiman"
+
+# Index dalam string rabbit
+# J = 0
+# a = 1
+# c = 2
+# dan seterusnya
+
+print(rabbit)
+
+# Mengambil index ke dua dari string, urutan ke tiga, yaitu c
+print(rabbit[2])
+
+# Mengambil index terakhir dari string, urutan terakhir yaitu n
+print(rabbit[-1])
