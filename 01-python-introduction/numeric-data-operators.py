8ae0d759d1adfdd6024731aea0ac33837997a0c3
NeoGlanding <neoglanding@protonmail.com>
2021-09-20 16:17:28 +0700

    numeric operators

---
+++ 01-python-introduction/numeric-data-operators.py
@@ -0,0 +1,15 @@
+# Tipe data numerik pada Python ada 3
+# 1. Integer => 2, 3, 4, dst
+# 2. Float => 1.5, 1.4, 213.3213, 3123. dst
+# 3. Complex
+
+# Numeric Operators
+a = 24
+b = 4
+
+print(a + b) # Pertambahan
+print(a - b) # Pengurangan
+print(a * b) # Perkalian
+print(a / b) # Pembagian yang menghasilkan float
+print(a // b) # Pembagian yang menghasilkan integer, dibulatkan kebawah
+print (a % b) # Modulus
