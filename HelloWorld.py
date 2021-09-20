f35d1876a3df6ac0fe7a26717ccac79702350da6
NeoGlanding <neoglanding@protonmail.com>
2021-09-20 13:45:32 +0700

    strings in python

--- HelloWorld.py
+++
@@ -1,4 +0,0 @@
-print('Hello World') #Print adalah fungsi, hello world adalah argumen
-print(1 + 2)
-print()
-print('The end', 'But can take more than one paramater') #print bisa mengambil lebih dari 1 argumen
