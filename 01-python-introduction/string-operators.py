6a58a70407d640d2cbb38ec2c05fbeee9754f21b
NeoGlanding <neoglanding@protonmail.com>
2021-09-20 18:42:16 +0700

    string operators

---
+++ 01-python-introduction/string-operators.py
@@ -0,0 +1,15 @@
+string1 = "My "
+string2 = "name "
+string3 = "is "
+string4 = "Ihsan"
+
+greeting = "Hello "
+
+# Cara pertama
+print(string1 + string2 + string3 + string4)
+
+# Cara kedua
+print("My " "name " "is " "Ihsan")
+
+# Mengulang string hingga beberapa kali
+print(greeting * 5)
